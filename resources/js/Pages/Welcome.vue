d65028f86dcc54960546bc06efacde6318f05cf2
Omar IZEM <contact@izem.dev>
2024-03-29 10:25:59 +0000

    chore: setup the project

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DefaultLayout from '@/Layouts/DefaultLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <DefaultLayout>
+        <Head title="Welcome" />
+
+        <div>
+            <h1>Welcome!!</h1>
+        </div>
+    </DefaultLayout>
+</template>
